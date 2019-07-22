7854ff56c07417bc746cb4c45dff98ef6ae58e2a
Alex Chaplinsky <alchaplinsky@gmail.com>
2019-07-23 00:45:01 +0200

    Add more tests

---
+++ test/features/entries/search.spec.js
@@ -0,0 +1,45 @@
+const { beforeHelper, afterHelper } = require('./../../helper')
+
+describe('Search credential entries', function() {
+  this.timeout(10000)
+
+  describe('user deletes credentials entry', () => {
+    before(() => beforeHelper({ storage: 'collection' }))
+
+    after(() => afterHelper())
+
+    it('shows credentials', () => {
+      return expect(
+        app.client
+          .setValue('input[type=password]', 'password')
+          .keys("\uE007")
+          .waitForExist('.body .list .entry')
+          .isExisting('.list .entry:nth-child(3)')
+      ).to.eventually.equal(true)
+    })
+    
+    it('filters entries', () => {
+      return expect(
+        app.client
+          .setValue('.search input', 'in')
+          .isExisting('.list .entry:nth-child(2)')
+      ).to.eventually.equal(false)
+    })
+    
+    it('shows only matching entries', () => {
+      return expect(
+        app.client
+          .setValue('.search input', 'in')
+          .getText('.list .entry:nth-child(1)')
+      ).to.eventually.equal('Instagram\nanotheruser')
+    })
+    
+    // it('adds entries back on clear filter', () => {
+    //   return expect(
+    //     app.client
+    //       .clearElement('.search input')
+    //       .isExisting('.list .entry:nth-child(3)')
+    //   ).to.eventually.equal(true)
+    // })
+  })
+})
