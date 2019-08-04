53abf38f16f8429b827260a20e7902d9207a1b26
Alex Chaplinsky <alchaplinsky@gmail.com>
2019-08-04 16:46:55 +0200

    Add more services icons

--- src/renderer/javascripts/defaults/icons.js
+++ src/renderer/javascripts/defaults/icons.js
@@ -1,15 +1,47 @@
 import DefaultIcon from 'web/default.svg'
+import AppleIcon from 'web/apple.svg'
+import AmazonIcon from 'web/amazon.svg'
+import BehanceIcon from 'web/behance.svg'
+import DribbbleIcon from 'web/dribbble.svg'
 import DropboxIcon from 'web/dropbox.svg'
 import FacebookIcon from 'web/facebook.svg'
 import InstagramIcon from 'web/instagram.svg'
 import YoutubeIcon from 'web/youtube.svg'
 import GithubIcon from 'web/github.svg'
 import GoogleIcon from 'web/google.svg'
+import LinkedinIcon from 'web/linkedin.svg'
+import MicrosoftIcon from 'web/microsoft.svg'
+import NetflixIcon from 'web/netflix.svg'
+import PaypalIcon from 'web/paypal.svg'
+import PinterestIcon from 'web/pinterest.svg'
+import RedditIcon from 'web/reddit.svg'
+import SkypeIcon from 'web/skype.svg'
+import SnapchatIcon from 'web/snapchat.svg'
+import SpotifyIcon from 'web/spotify.svg'
+import TwitterIcon from 'web/twitter.svg'
+import VimeoIcon from 'web/vimeo.svg'
+import WordpressIcon from 'web/wordpress.svg'
 
 export default {
   default: {
     color: '#4792ff',
     icon: DefaultIcon
+  },
+  amazon: {
+    color: '#FF9900',
+    icon: AmazonIcon
+  },
+  apple: {
+    color: '#A3AAAE',
+    icon: AppleIcon
+  },
+  behance: {
+    color: '#053eff',
+    icon: BehanceIcon
+  },
+  dribbble: {
+    color: '#ea4c89',
+    icon: DribbbleIcon
   },
   dropbox: {
     color: '#0060ff',
@@ -34,5 +66,53 @@ export default {
   youtube: {
     color: '#FF0000',
     icon: YoutubeIcon
+  },
+  linkedin: {
+    color: '#0077B5',
+    icon: LinkedinIcon
+  },
+  microsoft: {
+    color: '#F25022',
+    icon: MicrosoftIcon
+  },
+  netflix: {
+    color: '#E50914',
+    icon: NetflixIcon
+  },
+  paypal: {
+    color: '#00457C',
+    icon: PaypalIcon
+  },
+  pinterest: {
+    color: '#E50914',
+    icon: PinterestIcon
+  },
+  reddit: {
+    color: '#ff4500',
+    icon: RedditIcon
+  },
+  skype: {
+    color: '#BD081C',
+    icon: SkypeIcon
+  },
+  snapchat: {
+    color: '#ffd200',
+    icon: SnapchatIcon
+  },
+  spotify: {
+    color: '#1DB954',
+    icon: SpotifyIcon
+  },
+  twitter: {
+    color: '#1DA1F2',
+    icon: TwitterIcon
+  },
+  vimeo: {
+    color: '#19B7EA',
+    icon: VimeoIcon
+  },
+  wordpress: {
+    color: '#282828',
+    icon: WordpressIcon
   }
 }
