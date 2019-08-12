4ace8a3c54cd6833bbc373d42d6f895dfb87d608
Alex Chaplinsky <alchaplinsky@gmail.com>
2019-08-12 10:32:19 +0200

    Use light tray icon for non mac os

--- src/main/tray/index.js
+++ src/main/tray/index.js
@@ -1,10 +1,12 @@
 import path from 'path'
 import electron from 'electron'
 import trayIcon from 'iconTemplate@2x.png'
+import lightTrayIcon from 'iconLightTemplate@2x.png'
 
 export default class Tray extends electron.Tray {
   constructor(app) {
-    super(path.resolve(__dirname, trayIcon))
+    const icon = process.platform === 'darwin' ? trayIcon : lightTrayIcon
+    super(path.resolve(__dirname, icon))
     this.app = app
     this.setToolTip(CONFIG.name)
     this.on('click', () => this.app.window.show())
