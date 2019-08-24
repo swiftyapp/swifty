25de2f4d7c5a27ae8177675e96754dcce90f70a7
Alex Chaplinsky <alchaplinsky@gmail.com>
2019-08-24 14:01:34 +0200

    Add Unhandled rejection

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,17 @@
 import dotenv from 'dotenv'
 import Swifty from './application'
+import unhandled from 'electron-unhandled'
+import { openNewGitHubIssue, debugInfo } from 'electron-util'
+
+unhandled({
+  reportButton: error => {
+    openNewGitHubIssue({
+      user: 'swiftyapp',
+      repo: 'swifty',
+      body: `\`\`\`\n${error.stack}\n\`\`\`\n\n---\n\n${debugInfo()}`
+    })
+  }
+})
 
 dotenv.config()
 
