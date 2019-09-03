05f26bbbc7ca14b61a4a025b00f0a9bf16f09b13
Alex Chaplinsky <alchaplinsky@gmail.com>
2019-09-03 23:43:25 +0200

    Replace storage

--- src/main/sync/gdrive/auth/client.js
+++ src/main/sync/gdrive/auth/client.js
@@ -2,12 +2,10 @@ import path from 'path'
 import { google } from 'googleapis'
 import { DateTime } from 'luxon'
 import AuthWindow from '../../../window/auth'
-import Strg from './storage'
 import Storage, { appDir } from '../../../storage'
 
 export default class Client {
   constructor() {
-    this.strg = new Strg()
     this.storage = new Storage()
     this.auth = new google.auth.OAuth2(
       process.env.GOOGLE_OAUTH_CLIENT_ID,
@@ -64,11 +62,16 @@ export default class Client {
 
   storeTokens(tokens) {
     if (tokens.refresh_token) {
-      this.strg.write('refresh_token', tokens.refresh_token)
+      this.storage.write(
+        path.join(appDir(), 'access_token.json').tokens.refresh_token
+      )
       delete tokens.refresh_token
     }
     this.auth.setCredentials(tokens)
-    this.strg.write('access_token', tokens)
+    this.storage.write(
+      path.join(appDir(), 'access_token.json'),
+      JSON.stringify(tokens)
+    )
   }
 
   authUrl() {
