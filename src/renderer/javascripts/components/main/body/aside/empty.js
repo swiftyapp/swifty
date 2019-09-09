4d372caaa69bdc9e33d8fae7156ccfe4d20c7e8b
Alex Chaplinsky <alchaplinsky@gmail.com>
2019-09-09 18:19:50 +0200

    Replace empty state image

--- src/renderer/javascripts/components/main/body/aside/empty.js
+++ src/renderer/javascripts/components/main/body/aside/empty.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Safe from 'safe.svg'
+import Security from 'security.svg'
 
 export default () => {
   return (
     <div className="aside">
       <div className="empty">
-        <Safe width={300} height={300} />
+        <Security width={300} height={300} />
         <p>Create your first secure password entry</p>
       </div>
     </div>
