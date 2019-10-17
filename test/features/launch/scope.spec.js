5ddb6615ddee566a697912e09bdfe3476231f9b5
Alex Chaplinsky <alchaplinsky@gmail.com>
2019-10-17 17:36:21 +0300

    Fix tests

--- test/features/launch/scope.spec.js
+++ test/features/launch/scope.spec.js
@@ -12,64 +12,66 @@ describe('Scope switching', function() {
       return expect(
         app.client
           .setValue('input[type=password]', 'password')
-          .keys("\uE007")
+          .keys('\uE007')
           .waitForExist('.sidebar')
-          .isExisting('.switcher .item:nth-child(3)')
+          .isExisting('.switcher .tooltip-context:nth-child(3)')
       ).to.eventually.equal(true)
     })
-    
+
     it('highlights logins scope as current', () => {
       return expect(
-        app.client.isExisting('.switcher .item:nth-child(1).current')
+        app.client.isExisting(
+          '.switcher .tooltip-context:nth-child(1) .item.current'
+        )
       ).to.eventually.equal(true)
     })
-    
+
     it('displays entries for given scope', () => {
       return expect(
         app.client.getText('.body .list .entry')
       ).to.eventually.equal('Example\nmyuser')
     })
-    
+
     it('switches scope to notes', () => {
       return expect(
         app.client
-          .click('.switcher .item:nth-child(2)')
-          .isExisting('.switcher .item:nth-child(2).current')
+          .click('.switcher .tooltip-context:nth-child(2)')
+          .isExisting('.switcher .tooltip-context:nth-child(2) .item.current')
       ).to.eventually.equal(true)
     })
-    
+
     it('displays entries for given scope', () => {
-      return expect(
-        app.client.getText('.body .list')
-      ).to.eventually.equal('No Items')
+      return expect(app.client.getText('.body .list')).to.eventually.equal(
+        'No Items'
+      )
     })
-    
+
     it('switches scope to cards', () => {
       return expect(
         app.client
-          .click('.switcher .item:nth-child(3)')
-          .isExisting('.switcher .item:nth-child(3).current')
+          .click('.switcher .tooltip-context:nth-child(3)')
+          .isExisting('.switcher .tooltip-context:nth-child(3) .item.current')
       ).to.eventually.equal(true)
     })
-    
+
     it('displays entries for given scope', () => {
-      return expect(
-        app.client.getText('.body .list')
-      ).to.eventually.equal('No Items')
+      return expect(app.client.getText('.body .list')).to.eventually.equal(
+        'No Items'
+      )
     })
-    
+
     it('switches scope back to logins', () => {
       return expect(
         app.client
-          .click('.switcher .item:nth-child(1)')
-          .isExisting('.switcher .item:nth-child(1).current')
+          .click('.switcher .tooltip-context:nth-child(1)')
+          .isExisting('.switcher .tooltip-context:nth-child(1) .item.current')
       ).to.eventually.equal(true)
     })
-    
+
     it('displays entries for given scope', () => {
-      return expect(
-        app.client.getText('.body .list')
-      ).to.eventually.equal('Example\nmyuser')
+      return expect(app.client.getText('.body .list')).to.eventually.equal(
+        'Example\nmyuser'
+      )
     })
   })
 })
