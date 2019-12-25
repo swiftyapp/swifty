e8986ca14494e6b2c8ab57b6997f69308e1ad73d
Alex Chaplinsky <alchaplinsky@gmail.com>
2019-12-25 21:44:09 +0800

    Fix filtering term

--- src/renderer/javascripts/components/main/header/search.js
+++ src/renderer/javascripts/components/main/header/search.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import SearchIcon from 'search.svg'
 
 const Search = () => {
   const dispatch = useDispatch()
+  const filterTerm = useSelector(state => state.filters.query)
 
   const filterItems = event => {
     dispatch({ type: 'SET_FILTER_QUERY', query: event.target.value })
@@ -12,7 +13,12 @@ const Search = () => {
   return (
     <div className="search">
       <SearchIcon width="16" height="16" className="search-icon" />
-      <input type="text" placeholder="Search" onChange={filterItems} />
+      <input
+        type="text"
+        placeholder="Search"
+        value={filterTerm}
+        onChange={filterItems}
+      />
     </div>
   )
 }
