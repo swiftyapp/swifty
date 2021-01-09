4474d89f4698a843fb249ca57cd200202bea32c2
Alex Chaplinsky <alchaplinsky@gmail.com>
2021-01-09 14:39:08 +0100

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
-            query: {
+            options: {
               presets: ['@babel/react', '@babel/preset-env']
             }
           }
